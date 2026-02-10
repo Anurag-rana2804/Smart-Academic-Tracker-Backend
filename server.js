4f6cd5075169c73b836d6faaa340b06651bb06e7
unknown <ranaanurag2804@gmail.com>
2026-02-10 20:49:38 +0530

    Fixed Render port config

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ mongoose
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () =>
-  console.log("Server running on port 5000 ✅")
-);
+const PORT =
+  process.env.PORT || 5000;
 
+app.listen(PORT, () =>
+  console.log(
+    `Server running on port ${PORT} ✅`
+  )
+);
